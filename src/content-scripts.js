b05b3671642944e6baaf3bde1c1e6f7821658413
Al Cheung <cangzan@gmail.com>
2018-10-13 14:46:20 +0800

    Fix: get extension id in content scripts.

--- src/content-scripts.js
+++ src/content-scripts.js
@@ -1,6 +1,4 @@
-const EXTENSION_ID = 'ggopahneggnddhomgjomidldgjdedhak'
-
+const EXTENSION_ID = chrome.runtime.id
 const code = document.querySelector('#oauth_pin > p > kbd > code').innerText
-
 chrome.runtime.sendMessage(EXTENSION_ID, { [EXTENSION_ID]: code })
 
